ba5b9bdda716ba2664660808b5563651dd4f4258
Gary White <glw494@gmail.com>
2025-01-21 15:52:06 -0800

    Search functionality

---
+++ src/app/api/search/route.ts
@@ -0,0 +1,57 @@
+import { NextResponse } from "next/server";
+
+import { auth0 } from "@/lib/auth0";
+import prisma from "@/lib/prisma";
+
+export async function GET(req: Request) {
+  try {
+    const session = await auth0.getSession();
+
+    if (!session) throw new Error("User is unauthorized");
+
+    const organizationId = session?.user.org_id;
+    const url = new URL(req.url);
+    const query = url.searchParams.get("q") || "";
+
+    if (!query) {
+      return new Response(JSON.stringify([]), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const people = await prisma.person.findMany({
+      where: {
+        organizationId,
+        OR: [
+          { fullName: { contains: query, mode: "insensitive" } },
+          { email: { contains: query, mode: "insensitive" } },
+          { phone: { contains: query, mode: "insensitive" } },
+          { streetAddress: { contains: query, mode: "insensitive" } },
+          { city: { contains: query, mode: "insensitive" } },
+          { state: { contains: query, mode: "insensitive" } },
+          { postalCode: { contains: query, mode: "insensitive" } },
+          { gender: { contains: query, mode: "insensitive" } },
+          { ageRange: { contains: query, mode: "insensitive" } },
+          { maritalStatus: { contains: query, mode: "insensitive" } },
+        ],
+      },
+      include: {
+        Household: true,
+        lists: true,
+        groups: true,
+        donations: true,
+      },
+    });
+
+    return new Response(JSON.stringify(people), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message },
+      { status: error.status || 500 }
+    );
+  }
+}
